37e3fed47cf916730ff5e5a0ec13349b72ed6eb3
illeontev <illeontev@mail.ru>
2022-04-07 23:52:01 +0300

    Move secret key to .env

--- proxy_project/settings.py
+++ proxy_project/settings.py
@@ -9,8 +9,12 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
+
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-elk(w1ptnj0-668y0mu__unp+@!t0a#96g+ns#41tkh@lz0$k2'
+# SECRET_KEY = 'django-insecure-elk(w1ptnj0-668y0mu__unp+@!t0a#96g+ns#41tkh@lz0$k2'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
